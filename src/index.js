699b25956a1adf2a3522ef304c9fecddb462c9f1
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2020-10-18 12:58:44 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(el => el.join(''));
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if(str.includes(bracketsConfig[i])) {
+        str = str.replace(bracketsConfig[i], '');
+        i=-1;
+      }
+    }
+    
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
